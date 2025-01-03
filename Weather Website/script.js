95f49f45631b22f2103eb2ed58412551305eadf3
Huzaifa Abid <huzaifaabid586@gmail.com>
2025-01-03 05:05:12 +0500

    Second Commit

--- Weather Website/script.js
+++ Weather Website/script.js
@@ -39,7 +39,7 @@ const descriptionIcon = document.querySelector(".description i");
 
 function updateWeatherUI(data) {
     cityElement.textContent = data.name;
-    temperature.textContent = `${Math.round(data.main.temp)}°C`;
+    temperature.textContent = `${Math.round(data.main.temp)}`;
     windSpeed.textContent = `${data.wind.speed} km/h`;
     humidity.textContent = `${data.main.humidity}%`;
     visibility.textContent = `${data.visibility / 1000} km`;
